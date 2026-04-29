6fb929b8ebbd91bb0fba5f6782bdeb8ef6a888af
Sagar Panwar <sagarpanwer11@gmail.com>
2026-04-29 23:59:26 +0530

    Update Two_Sum_1.java

--- EASY/Two_Sum_1.java
+++ EASY/Two_Sum_1.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> pairIdx = new HashMap<>();
+        Map<Integer, Integer> pair = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
-            if (pairIdx.containsKey(target - num)) {
-                return new int[] { i, pairIdx.get(target - num) };
+            if (pair.containsKey(target - num)) {
+                return new int[] { i, pair.get(target - num) };
             }
-            pairIdx.put(num, i);
+            pair.put(num, i);
         }
 
         return new int[] {};        
