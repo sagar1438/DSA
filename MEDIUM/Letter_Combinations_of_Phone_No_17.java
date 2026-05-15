a30deaae2272e29bbe0d0dc0ef2db6aa1e12a459
sagar <sagarpanwer11@gmail.com>
2026-05-15 12:21:20 +0530

    LeetCode-17

---
+++ MEDIUM/Letter_Combinations_of_Phone_No_17.java
@@ -0,0 +1,22 @@
+class Solution {
+    static String[] key = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    public List<String> letterCombinations(String digits) {
+        List<String> ll = new ArrayList<>();
+        if (digits.isEmpty()) return ll;
+		Print(digits, "",ll);
+        return ll;
+    }
+    public static void Print(String ques, String ans,List<String> ll) {
+		if (ques.length() == 0) {
+			//System.out.println(ans);
+			ll.add(ans);
+			return;
+		}
+		char ch = ques.charAt(0);
+		String getString = key[ch - '0'];
+		for (int i = 0; i < getString.length(); i++) {
+			Print(ques.substring(1), ans + getString.charAt(i),ll);
+		}
+
+	}
+}
